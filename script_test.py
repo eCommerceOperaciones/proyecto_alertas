7e1d716de8f96dd2372d879f8e196a3d0dcf97b2
Rodrigo <ecommerceoperaciones01@gmail.com>
2025-12-07 07:30:47 +0100

    teste

---
+++ script_test.py
@@ -0,0 +1,14 @@
+from selenium import webdriver
+from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.firefox.service import Service
+
+options = Options()
+options.add_argument("--headless")
+driver = webdriver.Firefox(
+    service=Service("/usr/bin/geckodriver"), 
+    options=options
+)
+
+driver.get("https://www.google.com")
+print(driver.title)
+driver.quit()
